b64db3eaa265c9caac8336db35adf2169b946a4e
undertangoclub <pablocieslik@gmail.com>
2025-08-16 14:28:34 -0300

    test

--- registrar_visita.php
+++ registrar_visita.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=UTF-8');
 header('Cache-Control: no-store');
+ini_set('display_errors', 0); // pon 1 si quieres ver errores
 
 $archivo = __DIR__ . '/visitas.txt';
 if (!file_exists($archivo)) file_put_contents($archivo, '0');
@@ -9,4 +10,4 @@ $visitas = (int)trim(@file_get_contents($archivo));
 $visitas++;
 file_put_contents($archivo, (string)$visitas, LOCK_EX);
 
-echo json_encode(['ok'=>true,'visitas'=>$visitas]);
+echo json_encode(['ok' => true, 'visitas' => $visitas]);
